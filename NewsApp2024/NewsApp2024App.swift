6864433ac6d219a8ac693df9a9220eb7c94f1436
lingyoudao <lingyoudao@hotmail.com>
2024-10-02 08:04:57 +0800

    Initial Commit

---
+++ NewsApp2024/NewsApp2024App.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp2024App.swift
+//  NewsApp2024
+//
+//  Created by 凌有道 on 2024/10/2.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewsApp2024Tests/NewsApp2024Tests.swift
@@ -0,0 +1,33 @@
+//
+//  NewsApp2024Tests.swift
+//  NewsApp2024Tests
+//
+//  Created by 凌有道 on 2024/10/2.
+//
+
+import XCTest
+@testable import NewsApp2024
+
+class NewsApp2024Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
